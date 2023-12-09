ceb39b06d20f7597faf1c14128342d8f2e2d698d
d-as <notdasdas@gmail.com>
2023-12-09 22:23:22 +0200

    Clean up

--- solutions/day-09.ts
+++ solutions/day-09.ts
@@ -1,24 +1,17 @@
-import { first, getInputLines, last, logResults } from "../util.js";
+import { firstDefined, getInputLines, lastDefined, logResults } from "../util.js";
 
 const input = getInputLines(import.meta);
 
-const histories = input.map(line => (
-  line.split(' ').map(Number)
-));
+const histories = input.map(line => line.split(' ').map(Number));
 
 const differences = histories.map(history => {
   const diffs: number[][] = [history];
 
   while (true) {
-    const diff = last(diffs);
+    const diff = lastDefined(diffs);
+    diffs.push(diff.slice(1).map((n, i) => n - diff[i]));
 
-    diffs.push(
-      diff.slice(1).map((n, i) => (
-        n - diff[i]
-      ))
-    );
-
-    if (last(diffs).every(n => n === 0)) {
+    if (lastDefined(diffs).every(n => n === 0)) {
       break;
     }
   }
@@ -28,34 +21,23 @@ const differences = histories.map(history => {
 
 const predictions1 = differences.map(diffs => (
   diffs
-    .map(last)
+    .map(lastDefined)
     .reduce((a, b) => a + b)
 ));
 
 const predictions2 = differences
   .map(diffs => {
     let prediction = 0;
-
-    let diff =
-      diffs
-        .reverse()
-        .map(first);
+    let diff = diffs.reverse().map(firstDefined);
 
     while (diff.length >= 2) {
       const [a, b] = diff;
       prediction = b - a;
-
-      diff = [
-        prediction,
-        ...diff.slice(2),
-      ];
+      diff = [prediction, ...diff.slice(2)];
     }
 
     return prediction;
   });
 
-const result1 = predictions1.reduce((a, b) => a + b);
 
-const result2 = predictions2.reduce((a, b) => a + b);
-
-logResults(result1, result2);
+logResults(...[predictions1, predictions2].map(predictions => predictions.reduce((a, b) => a + b)));
