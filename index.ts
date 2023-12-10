ecde8565a871ceee4ed78ac96bd7c64f4309a750
d-as <notdasdas@gmail.com>
2023-12-10 17:58:40 +0200

    Try running latest solution again if input file doesn't exist

--- index.ts
+++ index.ts
@@ -87,48 +87,58 @@ if (SESSION_COOKIE && inputFilesToBeFetched.length > 0) {
     });
 }
 
-const solutionFiles = fs.readdirSync(SOLUTIONS_PATH);
+const runLatestSolution = async (allowRetry = true): Promise<void> => {
+  const solutionFiles = fs.readdirSync(SOLUTIONS_PATH);
 
-const solutionFilesToBeCreated = range(releasedPuzzleCount, 1)
-  .filter(day => !solutionFileExists(day, solutionFiles))
-  .map(getSolutionFileName);
+  const solutionFilesToBeCreated = range(releasedPuzzleCount, 1)
+    .filter(day => !solutionFileExists(day, solutionFiles))
+    .map(getSolutionFileName);
 
-if (solutionFilesToBeCreated.length > 0) {
-  const template = fs.readFileSync(path.join(SOLUTIONS_PATH, SOLUTION_TEMPLATE_NAME));
+  if (solutionFilesToBeCreated.length > 0) {
+    const template = fs.readFileSync(path.join(SOLUTIONS_PATH, SOLUTION_TEMPLATE_NAME));
 
-  console.log(
-    `Creating ${solutionFilesToBeCreated.length} solution file${plural(solutionFilesToBeCreated)}`
-  );
+    console.log(
+      `Creating ${solutionFilesToBeCreated.length} solution file${plural(solutionFilesToBeCreated)}`
+    );
 
-  solutionFilesToBeCreated.forEach(solution => (
-    fs.writeFileSync(path.join(SOLUTIONS_PATH, solution), template)
-  ));
-}
-
-const sortByFileName = (a: string, b: string) => getDayFromFilename(b) - getDayFromFilename(a);
-
-const [latestSolution] = fs.readdirSync(SOLUTIONS_PATH)
-  .filter(file => file.match(new RegExp(`${SOLUTION_PREFIX}\\d{2}`)))
-  .sort(sortByFileName);
-
-if (latestSolution) {
-  const inputFileExists = fs.existsSync(path.join(INPUT_PATH, getInputFileName(getDayFromFilename(latestSolution))));
-
-  if (inputFileExists) {
-    console.log(`Executing ${latestSolution}:\n`);
-
-    exec(`tsx ${path.join(SOLUTIONS_PATH, latestSolution)}`, (error, stdout, stderr) => {
-      if (error) {
-        return console.error(error);
-      } else if (stdout) {
-        console.log(stdout);
-      } else if (stderr) {
-        console.error(stderr);
-      }
-    });
-  } else {
-    console.log('Missing input file. Try running the script again.');
+    solutionFilesToBeCreated.forEach(solution => (
+      fs.writeFileSync(path.join(SOLUTIONS_PATH, solution), template)
+    ));
   }
-} else {
-  console.log('No solutions found');
-}
+
+  const sortByFileName = (a: string, b: string) => getDayFromFilename(b) - getDayFromFilename(a);
+
+  const [latestSolution] = fs.readdirSync(SOLUTIONS_PATH)
+    .filter(file => file.match(new RegExp(`${SOLUTION_PREFIX}\\d{2}`)))
+    .sort(sortByFileName);
+
+  if (latestSolution) {
+    const inputFileExists = fs.existsSync(path.join(INPUT_PATH, getInputFileName(getDayFromFilename(latestSolution))));
+
+    if (inputFileExists) {
+      console.log(`Executing ${latestSolution}:\n`);
+
+      exec(`tsx ${path.join(SOLUTIONS_PATH, latestSolution)}`, (error, stdout, stderr) => {
+        if (error) {
+          return console.error(error);
+        } else if (stdout) {
+          console.log(stdout);
+        } else if (stderr) {
+          console.error(stderr);
+        }
+      });
+    } else {
+      if (allowRetry) {
+        console.log('Missing input file, trying again in 1 second');
+        await new Promise(resolve => setTimeout(resolve, 1000));
+        runLatestSolution(false);
+      } else {
+        console.log('Missing input file');
+      }
+    }
+  } else {
+    console.log('No solutions found');
+  }
+};
+
+runLatestSolution();
